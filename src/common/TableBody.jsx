6de1c69525d0c7a8037588df27581c0569f3e68b
ritik kumar <ritikkumarkashyap9@gmail.com>
2024-10-04 13:17:53 +0530

    Sorting -Unique Key

--- src/common/TableBody.jsx
+++ src/common/TableBody.jsx
@@ -2,19 +2,32 @@ import React, { Component } from "react";
 import _ from "lodash";
 
 export default class TableBody extends Component {
+
+
   renderCell = (item, column) => {
     if (column.content) return column.content(item);
-    return _.get(item, column.path);
-  };
+      return _.get(item, column.path);
+      
+    };
+    
+
+  createKey = (item, column) => {
+      return item._id + (column.path || column.key);
+      
+    };
+    
 
   render() {
-    const { data, columns } = this.props;
+      const { data, columns } = this.props;
+      
     return (
       <tbody>
         {data.map((item) => (
-          <tr>
+          <tr key={item._id}>
             {columns.map((column) => (
-              <td>{this.renderCell(item, column)}</td>
+              <td key={this.createKey(item, column)}>
+                {this.renderCell(item, column)}
+              </td>
             ))}
           </tr>
         ))}
