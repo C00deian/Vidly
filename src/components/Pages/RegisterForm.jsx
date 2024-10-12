31494f4a549bbccde5746b5871f1478076b3fc98
ritik kumar <ritikkumarkashyap9@gmail.com>
2024-10-12 16:22:12 +0530

    register user APi

--- src/components/Pages/RegisterForm.jsx
+++ src/components/Pages/RegisterForm.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Joi from "joi";
 import Form from "../../common/Form";
-
+import { toast } from "react-toastify";
+import * as userService from '../../services/userService'
 export default class RegisterForm extends Form {
   state = {
     data: {
@@ -17,17 +18,31 @@ export default class RegisterForm extends Form {
 
     password: Joi.string()
       .required()
-      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      // .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
       .min(5)
       .label("Password"),
 
     name: Joi.string().required().label("Name"),
   });
-
-  doSubmit = () => {
-    //call the server
-    console.log("submited Register");
-    };
+  doSubmit = async () => {
+    try {
+      await userService.register(this.state.data);
+      toast.success("Registration successful!");
+      // Redirect or reset form here
+    } catch (ex) {
+      if (ex.response) {
+        if (ex.response.status === 404) {
+          // Display the toast notification for the user already registered
+          toast.error("User already registered.");
+        } else {
+          // Handle other errors
+          toast.error("An unexpected error occurred.");
+        }
+      } else {
+        toast.error("Network error: Unable to connect to the server.");
+      }
+    }
+  };
     
  
   render() {
--- src/services/genresService.js
+++ src/services/genresService.js
@@ -1,6 +1,6 @@
 import http from "./httpService";
 import {apiUrl} from "../config/config.json";
 
-export function getGenres() {
+ export function getGenres() {
   return http.get(`${apiUrl}/genres`);
 }
