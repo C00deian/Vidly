c4f9020c8b656567f7384a1c694e06d50925ea63
ritik kumar <ritikkumarkashyap9@gmail.com>
2024-10-13 23:15:27 +0530

    decoded user by token

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Movies from "./components/Movies";
-import {  Route, Routes,Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 import Customers from "./components/Pages/Customers";
 import Rentals from "./components/Pages/Rentals";
 import NoPage from "./components/pages/NoPage";
@@ -8,12 +9,25 @@ import MovieForm from "./components/Pages/MovieForm";
 import LoginForm from "./components/Pages/LoginForm";
 import RegisterForm from "./components/Pages/RegisterForm";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css'
-
-
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    try {
+      const jwt = localStorage.getItem("token");
+      const decodedUser = jwtDecode(jwt);
+      setUser({ decodedUser });
+      
+    } catch (error) {
+      
+    }
+  },[])
+
+
   const routeCompnents = [
     { path: "/login", elements: <LoginForm /> },
     { path: "/movies", elements: <Movies /> },
@@ -22,21 +36,19 @@ function App() {
     { path: "/customers", elements: <Customers /> },
     { path: "/rentals", elements: <Rentals /> },
     { path: "/register", elements: <RegisterForm /> },
-
-
   ];
 
   return (
     <>
-      <ToastContainer/>
-      <Navbar />
+      <ToastContainer />
+      <Navbar user={user} />
       <main className="container">
         <Routes>
-        <Route path="/" element={<Navigate to="/movies" replace />} />
+          <Route path="/" element={<Navigate to="/movies" replace />} />
           {routeCompnents.map((route, index) => (
             <Route path={route.path} element={route.elements} key={index} />
           ))}
-             <Route path="*" element={<Navigate to="/not-found" replace />} />
+          <Route path="*" element={<Navigate to="/not-found" replace />} />
         </Routes>
       </main>
     </>
